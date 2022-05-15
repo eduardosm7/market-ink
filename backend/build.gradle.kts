52d1f6b7aeb1c7a46a5c1b7cf13806992e8c349e
eduardothemoura <eduardothemoura@gmail.com>
2022-05-15 21:46:47 +0200

    [#1] Add Postgres driver and connect with Heroku's Postgres

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	runtimeOnly("com.h2database:h2")
+	runtimeOnly("org.postgresql:postgresql")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
